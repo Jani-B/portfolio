2e6100d048357de23b554a42d1fb97c53a2ac71f
Jani <aijala.j@gmail.com>
2023-08-05 16:34:11 +0300

    added menu icon as hamburger and close igon X to javascript

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,8 +2,12 @@ function menu() {
     let x = document.getElementById('myTopnav');
     if (x.className === 'navigation tab') {
         x.className += ' responsive tab';
+        let menuChange = document.getElementById('hamb');
+        menuChange.innerHTML = 'X';
     } else {
         x.className = 'navigation tab';
+        let menuChange = document.getElementById('hamb');
+        menuChange.innerHTML = '☰';
     }
 };
 
